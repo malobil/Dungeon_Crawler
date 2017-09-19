fa6650b3d658e900f62212bbd607d64f85ffab61
PCP-TRISTAN\trist <tristanlavaud@hotmail.fr>
2017-09-19 17:27:01 +0200

    Prepa pour meca

---
+++ Assets/Scripts/Meca_Script/Script raycast.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scriptraycast : MonoBehaviour {
+{
+		void FixedUpdate()
+		{
+			RaycastHit hit;
+
+			if (Physics.Raycast(transform.position, -Vector3.up, out hit, 100.0f))
+				print("Found an object - distance: " + hit.distance);
+		}
+}
