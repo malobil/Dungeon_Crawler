8118f4df7cb0577cd9635d919ed161d79986f43b
Batisse Pierre <pierrebatisse33130@gmail.com>
2017-09-20 09:34:33 +0200

    Ajout script wilfried_HP pour gestion jauge de vie - Batisse Pierre 20/09/17 09h34

---
+++ Assets/Scripts/Wilfried_Script/Wilfried_HP.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Wilfried_HP : MonoBehaviour {
+
+    public Image healthBar;
+    public float wilfried_current_HP;
+    public float wilfried_max_HP;
+    public float damages_To_Wilfried;
+   
+	// Use this for initialization
+	void Start () {
+
+        LifeManager();
+	}
+
+    public void LifeManager()
+    {
+
+        healthBar.fillAmount = wilfried_current_HP / wilfried_max_HP;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("m"))
+        {   //Lorsque joueur appuie sur la touche "m"
+            wilfried_current_HP = wilfried_current_HP - damages_To_Wilfried;
+            LifeManager();
+        }
+	}
+}
