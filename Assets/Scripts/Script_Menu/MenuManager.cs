944c8124e006b8e2e6841559c726cc3cd52bc7c1
MSI\louis <lou_iis@live.fr>
2017-09-20 10:53:51 +0200

    menu UI : bouton quitter fonctionnel

---
+++ Assets/Scripts/Script_Menu/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    public void QuitMenu ()
+    {
+        Application.Quit();
+        Debug.Log("quit game");
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
