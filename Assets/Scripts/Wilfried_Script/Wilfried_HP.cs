73deb100ae143b07099647aedf4658d3b6b78dcf
Batisse Pierre <pierrebatisse33130@gmail.com>
2017-09-20 09:55:52 +0200

    ajout hp minimum et texte sur barre de vie - Batisse Pierre 20/09/17 09H55

--- Assets/Scripts/Wilfried_Script/Wilfried_HP.cs
+++ Assets/Scripts/Wilfried_Script/Wilfried_HP.cs
@@ -8,24 +8,23 @@ public class Wilfried_HP : MonoBehaviour {
     public Image healthBar;
     public float wilfried_current_HP;
     public float wilfried_max_HP;
+    public float wilfried_min_HP;
     public float damages_To_Wilfried;
-   
+    public Text hp;
 	// Use this for initialization
 	void Start () {
-
         LifeManager();
 	}
 
     public void LifeManager()
     {
-
         healthBar.fillAmount = wilfried_current_HP / wilfried_max_HP;
-
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown("m"))
+        hp.text = wilfried_current_HP.ToString() + "/" + wilfried_max_HP.ToString();
+        if (Input.GetKeyDown("m") & wilfried_current_HP > wilfried_min_HP)
         {   //Lorsque joueur appuie sur la touche "m"
             wilfried_current_HP = wilfried_current_HP - damages_To_Wilfried;
             LifeManager();
