0675c5de80682433242d2e3ed011703f9a61a600
Arthur Manoux <arthur.manoux@wanadoo.fr>
2017-09-20 14:18:15 +0200

    Scene test mécanisme vapor modif

--- Assets/Scripts/Script_Mécanismes/Jet de vapeur/Bouclevapeur.cs
+++ Assets/Scripts/Script_Mécanismes/Jet de vapeur/Bouclevapeur.cs
@@ -1,16 +1,56 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bouclevapeur : MonoBehaviour {
 
+	public ParticleSystem vapor;
+	public float cooldownVapor;
+	private float cooldownActive;
+	private bool vaporActive;
+
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		cooldownActive = cooldownVapor;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		CooldownVapor();
+		ParticuleOnOff();
 		
 	}
+
+	void CooldownVapor()
+	{
+
+		cooldownActive -= Time.deltaTime;
+
+		 if(cooldownActive <= 0f)
+		 {
+		 	vaporActive = !vaporActive;
+		 	cooldownActive = cooldownVapor;
+		 }
+
+	 }
+
+	void ParticuleOnOff ()
+
+	{
+
+		if(vaporActive == false)
+		{
+			vapor.Stop();
+		}
+		else if( vaporActive == true)
+		{
+
+			vapor.Play();
+		} 
+
+	}
+
 }
