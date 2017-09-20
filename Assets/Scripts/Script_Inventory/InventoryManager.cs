c37cb8d4abb1cfef4563e7f6223e39ca0d772296
OTAKU <otaku.suprem@gmail.com>
2017-09-20 18:16:50 +0200

    Update inventory script

--- Assets/Scripts/Script_Inventory/InventoryManager.cs
+++ Assets/Scripts/Script_Inventory/InventoryManager.cs
@@ -8,32 +8,29 @@ public class InventoryManager : MonoBehaviour {
 	public List<GameObject> imageSlot = new List<GameObject>() ;
 	public GameObject refImage ;
 	private bool haveSlot = false ;
-	Ray ray ;
-	RaycastHit hit ;
-
-	// Use this for initialization
-	void Start () 
-	{
-		
-	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		RaycastHit hit ;
 		// ClickOnItem
+		Ray raycast = Camera.main.ScreenPointToRay(Input.mousePosition);
 		if(Input.GetMouseButtonDown(0))
 		{
-			haveSlot = false ; // You can add an item in a slot
-			Debug.Log("haha") ;
-			for(int i = 0; i < slot.Count; i++) // Continue until slot limit is reached
-			{
-				Debug.Log("urrryyyy");
-				if(slot[i] == null && !haveSlot) // If there is no slot here 
+			if (Physics.Raycast(raycast,out hit,4) && (hit.collider.tag == "Item"))
+			{       
+				haveSlot = false ; // You can add an item in a slot
+				Debug.Log("haha") ;
+				for(int i = 0; i < slot.Count; i++) // Continue until slot limit is reached
 				{
-					Debug.Log("muda");
-					GameObject itemInSlot = Instantiate(refImage,imageSlot[i].transform) ; // Create an image of the item selected
-					slot[i] = itemInSlot;
-					haveSlot = true ;
+					Debug.Log("urrryyyy");
+					if(slot[i] == null && !haveSlot) // If there is no slot here 
+					{
+						Debug.Log("muda");
+						GameObject itemInSlot = Instantiate(refImage,imageSlot[i].transform) ; // Create an image of the item selected
+						slot[i] = itemInSlot;
+						haveSlot = true ;
+					}
 				}
 			}
 		}
