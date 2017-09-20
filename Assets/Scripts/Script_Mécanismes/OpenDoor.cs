0f4930ae172657ac2274fbcfe8a2b98c05b70391
PCP-TRISTAN\trist <tristanlavaud@hotmail.fr>
2017-09-20 11:07:02 +0200

    Beta du code de l'ouverture de la porte

---
+++ Assets/Scripts/Script_Mécanismes/OpenDoor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour 
+{
+
+void FixedUpdate () 
+	{
+		RaycastHit hit ;
+		if (Physics.Raycast(transform.position,transform.forward, out hit,4))
+		{
+		   gameObject.GetComponent<Animator>().SetTrigger("OpenDoor");
+		}
+	}
+}
+
