9ad857788502fd98a80da74bf8fdd158cc175e4a
MSI\louis <lou_iis@live.fr>
2017-09-20 09:18:57 +0200

    menu_UI_louis

---
+++ Assets/Scripts/Script_Menu_UI/quit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    void Quit()
+
+
+    {
+        Application.Quit();
+        Debug.Log("quitter le jeu");
+    }
+
+
+}
