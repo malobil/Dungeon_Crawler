e652a07fd9c2c6f5ac68d1e0c81867dff54dd996
PCP-TRISTAN\trist <tristanlavaud@hotmail.fr>
2017-09-21 14:42:38 +0200

    no message

---
+++ Assets/Scripts/Script_Mécanismes/PorteManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PorteManager : MonoBehaviour 
+{
+public void ToggleOuverturePorte () 
+		{
+			GetComponent<Animator>().SetTrigger("OpenDoor");
+		}
+}
