1ef5a0c43d9b8f602f58e10625a909b109075515
Killian Beguerie <killian.beguerie@e-artsup.net>
2017-09-21 10:13:48 +0200

    Trigger sound

---
+++ Assets/Scripts/Script_Event/Trigger_Sound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_Sound : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TriggerSound();
+	}
+	void TriggerSound (){
+		if (Input.GetKeyDown("r"))
+		{
+			GetComponent<AudioSource>().Play();
+		}
+	}
+}
