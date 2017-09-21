92ec8f2887e8fdee2bacd23eaaaf50e2933d470b
OTAKU <otaku.suprem@gmail.com>
2017-09-21 19:05:32 +0200

    Drag and drop debut de script

---
+++ Assets/Scripts/Script_Inventory/DragAndDrop.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour , IBeginDragHandler, IDragHandler, IEndDragHandler{
+	public static GameObject itemBeingDragged ;
+
+
+	#region IBeginDragHandler implementation
+
+	public void OnBeginDrag (PointerEventData eventData)
+	  {
+	  	itemBeingDragged = gameObject ;
+	  	startPosition = transform.position ;
+	  }
+
+	#endregion
+
+	#region IDragHandler implementation
+
+	public void OnDrag (PointerEventData eventData)
+	  {
+	  	throw new System.NotImplementedException () ;
+	  }
+
+	#endregion
+
+	#region IEndDragHandler implementation
+
+	public void OnEndDrag (PointerEventData eventData)
+	  {
+	  	itemBeingDragged = null ;
+	  }
+
+	#endregion
+}
