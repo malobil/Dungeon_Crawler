50fffd448d578b8ba72076d23d816fd7febb9b72
PCP-TRISTAN\trist <tristanlavaud@hotmail.fr>
2017-09-21 15:06:49 +0200

    no message

--- Assets/Scripts/Script_Mécanismes/CoffreManager.cs
+++ Assets/Scripts/Script_Mécanismes/CoffreManager.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class CoffreManager : MonoBehaviour 
 {
-private bool isOpen = false; 
+	private bool isOpen = false; 
 
-public void ToggleOuvertureCoffre () 
+	public void ToggleOuvertureCoffre () 
 	{
 		if (!isOpen)
 		{
 			GetComponent<Animator>().SetTrigger("OpenCoffre");
-			isOpen = true;
 		}
-		else 
+		else if (isOpen)
 		{
-			GetComponent<Animator>().SetTrigger("CloseDoor");
-			isOpen = false;		
+			GetComponent<Animator>().SetTrigger("CloseDoor");	
 		}
+	}
+
+	public void SetOpen (){
+		isOpen = true;
+		GetComponent<Animator>().ResetTrigger("OpenCoffre");
+	}
+
+	public void SetClosed (){
+		isOpen = false;
+		GetComponent<Animator>().ResetTrigger("CloseDoor");
 	}
 }
