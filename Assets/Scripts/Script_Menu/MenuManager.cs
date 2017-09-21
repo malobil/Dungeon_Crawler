3f710df2ecd3d7c6f04cef3773974f51a165e076
MSI\louis <lou_iis@live.fr>
2017-09-21 10:44:40 +0200

    menu pause programmé OK

--- Assets/Scripts/Script_Menu/MenuManager.cs
+++ Assets/Scripts/Script_Menu/MenuManager.cs
@@ -2,32 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class MenuManager : MonoBehaviour {
 
     public string sceneToLoad;
     private bool isPause = false;
+    public GameObject menuPause;
+    public GameObject menu;
 
-
-    public void QuitMenu ()
+    public void QuitMenuAccueil ()
    	{
         Application.Quit();
         Debug.Log("quit game");
     }
 
-	public void PlayMenu ()
+	public void PlayMenuAccueil ()
    	{
         SceneManager.LoadScene (sceneToLoad);
    	}
-
-    void PauseMenu ()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-            isPause = !isPause;
-
-       
-    }
-
 
 
 	// Use this for initialization
@@ -37,6 +28,22 @@ public class MenuManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPause = !isPause;
+            menuPause.SetActive(isPause);
+        } 
 	}
+
+    public void continuer ()
+    {
+        menuPause.SetActive(false);
+    }
+
+    public void quitterPause ()
+    {
+        menuPause.SetActive(false);
+        menu.SetActive(true);
+    }
 }
