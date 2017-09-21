096096372fa0b004767a8d8b2f8c0bfe6c05e4e1
Arthur Manoux <arthur.manoux@wanadoo.fr>
2017-09-21 16:42:16 +0200

    préfab porte simple

---
+++ Assets/Scripts/Script_Mécanismes/Animator_Event.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animator_Event : MonoBehaviour {
+	public PortesimpleManager myDoorManager;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SendOpenMessage()
+	{
+		myDoorManager.SetOpen();
+	}
+	public void SendCloseMessage()
+	{
+		myDoorManager.SetClosed();
+	}
+}
---
+++ Assets/Scripts/Script_Mécanismes/Porte.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Porte : MonoBehaviour 
+{
+public int range;
+
+void FixedUpdate () 
+	{
+		RaycastHit hit ;
+
+		if (Input.GetMouseButtonDown(0))
+        {
+          	Ray raycast = Camera.main.ScreenPointToRay(Input.mousePosition);
+           	if (Physics.Raycast(raycast,out hit,range))
+			{
+				if (hit.collider.tag == "Portesimple"){
+		   			hit.collider.GetComponent<PortesimpleManager>().ToggleOuverturePorte();
+		   		}
+			}
+			
+        }
+    }
+}
+
