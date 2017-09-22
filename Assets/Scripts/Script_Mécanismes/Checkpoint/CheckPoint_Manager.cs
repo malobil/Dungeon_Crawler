c200d64994b448c70af3614672354f09ec3a0d16
Mikael.PC <mikaeljouchoux@gmail.com>
2017-09-22 15:03:07 +0200

    Checkpoint death

---
+++ Assets/Scripts/Script_Mécanismes/Checkpoint/CheckPoint_Manager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint_Manager : MonoBehaviour 
+{
+
+
+	 void OnTriggerEnter(Collider col)
+   	{
+   		if (col.CompareTag("Player"))
+   		{
+    	 	col.GetComponent<Wilfried_Moves>().SetRespawn();
+
+    	 	Destroy(gameObject);
+
+        }
+
+	}
+
+}
