cd4f52fcc2eb75238bd01318353602a3cb3d431f
Jordan <j.drespincho@yahoo.com>
2017-09-22 11:54:18 +0200

    Update du prefab Mur Coffre avec loot d'un item

--- Assets/Scripts/Script_Loot/LootCoffreManager.cs
+++ Assets/Scripts/Script_Loot/LootCoffreManager.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class LootCoffreManager : MonoBehaviour {
 
+	public Transform coffrePosition;
+	private ScriptableItem itemToLoot;
+	public ItemInCoffreManager associateListScriptable;
+
 	// Use this for initialization
 	void Start () {
-		
+		int temp = Random.Range(0,associateListScriptable.listOfItem.Length);
+		itemToLoot = associateListScriptable.listOfItem[temp];
+		Instantiate (itemToLoot.itemPrefab, coffrePosition.position, Quaternion.identity);
 	}
 	
 	// Update is called once per frame
